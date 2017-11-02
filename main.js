af7309a47634d5322500e752436f0c98c95e2bb9
Bernard Strong <bstrong71@gmail.com>
2017-11-02 15:26:01 -0400

    added option to sum rolls

--- main.js
+++ main.js
@@ -9,8 +9,16 @@ printOutput = (message) => {
 rollDice = () => {
   let quantity = document.querySelector("#inputQuantity").value;
   let sides = document.querySelector("#inputSides").value;
+  let sumFlag = document.querySelector("#sumOption").checked;
+  let sum = 0;
 
   for (let i = 0; i < quantity; i++) {
-    printOutput(randomNumber(sides));
+    let num = randomNumber(sides);
+    sum += num;
+    printOutput(num);
+  }
+
+  if (sumFlag) {
+    printOutput("Sum of rolls = " + sum);
   }
 }
