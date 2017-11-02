2b3a1a9e44a1c9cdb91d7766cc1b477069ca7d4a
Sean McPherson <seanmcp25@gmail.com>
2017-11-02 17:44:03 -0400

    Tinker with formatting; increase font-size on output

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,15 @@ let sides = document.querySelector("#inputSides");
 let sumFlag = document.querySelector("#sumOption");
 let saveFlag = document.querySelector("#saveOption");
 
+let resultsArr = [];
+
 randomNumber = (sides) => Math.floor(Math.random() * sides) + 1;
 
 printOutput = (message) => {
   output.innerHTML += `<p>${message}</p>`;
+}
+clearOutput = () => {
+  output.innerHTML = "";
 }
 
 enableTotal = (e) => {
@@ -22,12 +27,13 @@ enableTotal = (e) => {
 rollDice = () => {
   let sum = 0;
   let content = output.innerHTML;
-  output.innerHTML = "";
+  clearOutput();
 
   for (let i = 0; i < quantity.value; i++) {
     let num = randomNumber(sides.value);
-    sum += num;
     printOutput(num);
+    sum += num;
+    resultsArr.push(num);
   }
 
   if (sumFlag.checked && quantity.value > 1) {
@@ -37,7 +43,7 @@ rollDice = () => {
     output.innerHTML += content;
   }
   if (quantity.value < 1 || sides.value < 2) {
-    output.innerHTML = "";
+    clearOutput();
     printOutput("I'm sorry, but I can't do that");
   }
 }
